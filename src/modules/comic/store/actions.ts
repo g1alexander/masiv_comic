57bd9caed97327c33d29b6d77c085b770f24ad20
g1alexander <alexander.granados.dev@gmail.com>
2023-10-05 10:15:35 -0500

    feat: set stars and page

--- src/modules/comic/store/actions.ts
+++ src/modules/comic/store/actions.ts
@@ -5,17 +5,30 @@ import { comic } from '../services'
 export const useActions = defineStore('comic.actions', () => {
   const state = useState()
 
-  const setComic = async (page: number): Promise<void> => {
+  const setComic = async (): Promise<void> => {
     state.isLoading = true
 
-    const response = await comic(page)
+    const response = await comic(state.page)
 
     state.comic = { ...state.comic, ...response }
 
     state.isLoading = false
   }
 
+  const setPage = (payload: number) => {
+    if (payload < 1) {
+      state.page = 1
+
+      return
+    }
+    state.page = payload
+  }
+
+  const setStar = (payload: number) => {
+    state.comic = { ...state.comic, stars: payload }
+  }
+
   // Note you are free to define as many internal functions as you want.
   // You only expose the functions that are returned.
-  return { setComic }
+  return { setComic, setPage, setStar }
 })
