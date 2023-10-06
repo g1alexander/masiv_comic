8f1f91578b634d887ec376a58c5db2658be19237
g1alexander <alexander.granados.dev@gmail.com>
2023-10-06 14:19:08 -0500

    test: mock fake data

--- tests/unit/mocks/data.ts
+++ tests/unit/mocks/data.ts
@@ -1,3 +1,5 @@
+import { createApp } from 'vue'
+
 export const comicData = {
   alt: "Don't we all.",
   img: 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg',
@@ -6,4 +8,32 @@ export const comicData = {
   title: 'Barrel - Part 1',
   transcript:
     "[[A boy sits in a barrel which is floating in an ocean.]]\nBoy: I wonder where I'll float next?\n[[The barrel drifts into the distance. Nothing else can be seen.]]\n{{Alt: Don't we all.}}"
+}
+
+export const comicAPIData = {
+  month: '1',
+  num: 1,
+  link: '',
+  year: '2006',
+  news: '',
+  safe_title: 'Barrel - Part 1',
+  transcript:
+    "[[A boy sits in a barrel which is floating in an ocean.]]\nBoy: I wonder where I'll float next?\n[[The barrel drifts into the distance. Nothing else can be seen.]]\n{{Alt: Don't we all.}}",
+  alt: "Don't we all.",
+  img: 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg',
+  title: 'Barrel - Part 1',
+  day: '1'
+}
+
+export const dataComponent = {
+  HomeInfo: createApp({
+    template: `<div></div>`
+  }) as any,
+  HomeActions: createApp({
+    template: `<div></div>`
+  }) as any,
+  comic: {
+    getIsLoading: true
+  } as any,
+  loader: '/src/assets/icons/loader.svg'
 }
