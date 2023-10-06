dcae79317f22f6251bb85ecc1a2ced7faaffa0c5
g1alexander <alexander.granados.dev@gmail.com>
2023-10-05 21:49:22 -0500

    test: indexedDB and service comic

---
+++ tests/unit/mocks/data.ts
@@ -0,0 +1,9 @@
+export const comicData = {
+  alt: "Don't we all.",
+  img: 'https://imgs.xkcd.com/comics/barrel_cropped_(1).jpg',
+  num: 1,
+  stars: 3,
+  title: 'Barrel - Part 1',
+  transcript:
+    "[[A boy sits in a barrel which is floating in an ocean.]]\nBoy: I wonder where I'll float next?\n[[The barrel drifts into the distance. Nothing else can be seen.]]\n{{Alt: Don't we all.}}"
+}
